651b7c1c76f9f3bc5e22b78407132489670834f9
vvvkor <vadimkor@yandex.ru>
2019-05-27 12:34:47 +0300

    docs

--- dist/d1.js
+++ dist/d1.js
@@ -1,4 +1,4 @@
-/*! d1css v1.2.36 https://github.com/vvvkor/d1 */
+/*! d1css v1.2.38 https://github.com/vvvkor/d1 */
 /* Enhancements for d1css microframework */
 
 (function(window, document, Element) {
