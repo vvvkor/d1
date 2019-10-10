6c88f389aa80d7f80a1d2c98055e799eee2d1ef6
vvvkor <vadimkor@yandex.ru>
2019-10-10 16:20:18 +0300

    colors order

--- dist/d1.js
+++ dist/d1.js
@@ -1,4 +1,4 @@
-/*! d1css v1.2.61 https://github.com/vvvkor/d1 */
+/*! d1css v1.2.62 https://github.com/vvvkor/d1 */
 /* Enhancements for d1css microframework */
 
 (function(window, document, Element) {
